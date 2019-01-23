754e05e9a3d73f7acf612652b60b3acf6e3fe8d8
Theo <theobasallaje@gmail.com>
2019-01-22 18:24:37 -0600

    some html structure

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,19 @@
 var database = firebase.database();
 
-
+//variables
+var options = $(".option");
+//player 1
 var playerOne;
+var p1Rock = $("#p1Rock");
+var p1Paper = $("#p1Paper");
+var p1Scissors = $("#p1Scissors");
+//player 2
 var playerTwo;
+var p2Rock = $("#p2Rock");
+var p2Paper = $("#p2Paper");
+var p2Scissors = $("#p2Scissors");
 
+//functions
 $("#request").on("click", function(){
     alert("request clicked");
 });
